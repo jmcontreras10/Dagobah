f1fa1e1d14e83516f2362477261fe3534a863b1e
mateocontreras <mateo.contreras@workrise.com>
2024-10-09 01:47:09 -0500

    feat: adding getOneById

--- src/main/kotlin/co/mateocontreras/dagobah/controllers/ExerciseController.kt
+++ src/main/kotlin/co/mateocontreras/dagobah/controllers/ExerciseController.kt
@@ -5,6 +5,7 @@ import co.mateocontreras.dagobah.services.ExerciseService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -19,5 +20,11 @@ class ExerciseController(
     @ResponseStatus(HttpStatus.OK)
     fun getExercises(): List<ExerciseDto> {
         return exerciseService.getExercises()
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getExerciseById(@PathVariable id: String): ExerciseDto {
+        return exerciseService.getExerciseById(id)
     }
 }
--- src/main/kotlin/co/mateocontreras/dagobah/services/ExerciseService.kt
+++ src/main/kotlin/co/mateocontreras/dagobah/services/ExerciseService.kt
@@ -3,10 +3,13 @@ package co.mateocontreras.dagobah.services;
 import co.mateocontreras.dagobah.controllers.dto.ExerciseDto
 import co.mateocontreras.dagobah.mappers.toDto
 import co.mateocontreras.dagobah.repositories.ExerciseRepository;
+import co.mateocontreras.dagobah.util.toObjectId
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 class ExerciseService (@Autowired val exerciseRepository: ExerciseRepository) {
         fun getExercises(): List<ExerciseDto> = exerciseRepository.findAll().map { it.toDto() }
+
+        fun getExerciseById(id: String): ExerciseDto = exerciseRepository.findById(id.toObjectId()).get().toDto()
 }
