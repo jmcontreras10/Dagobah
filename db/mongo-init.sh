4bccdb0b55f8d230da98214aaf3df1815f75b9f8
mateocontreras <mateo.contreras@workrise.com>
2024-10-10 01:17:06 -0500

    chore: final testings

--- db/mongo-init.sh
+++ db/mongo-init.sh
@@ -1,5 +1,16 @@
-apt-get update -y
-apt-get install nodejs -y
-apt-get install npm -y
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install 16
+nvm use 16
+nvm alias default 16
 npm install
-node init-mongo-data.js localhost:27018 mongodb://mongo:password@localhost:27018/dagobah
+node init-mongo-data.js mongodb://mongo:password@localhost:27018/dagobah
