ca941f0a2e1cc8fb727b4da912e2db09750594e8
mateocontreras <mateo.contreras@workrise.com>
2024-10-10 00:36:36 -0500

    chore: testings

--- db/mongo-init.sh
+++ db/mongo-init.sh
@@ -1,5 +1,8 @@
 sudo apt-get update
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
+sudo apt install curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install 16
+nvm use 16
+nvm alias default 16
 npm install
 node init-mongo-data.js localhost:27018 mongodb://mongo:password@localhost:27018/dagobah
