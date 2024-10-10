f1d792f185006f862db19b21efd81d31c101cbbe
mateocontreras <mateo.contreras@workrise.com>
2024-10-10 00:39:08 -0500

    chore: testings

--- db/mongo-init.sh
+++ db/mongo-init.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt install curl
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 nvm install 16
 nvm use 16
 nvm alias default 16
