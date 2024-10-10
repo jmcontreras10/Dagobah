fe072646724a2f9dd486cec79c56c6972985afb8
mateocontreras <mateo.contreras@workrise.com>
2024-10-10 00:23:47 -0500

    chore: testings

--- db/mongo-init.sh
+++ db/mongo-init.sh
@@ -1,5 +1,5 @@
-apt-get update -y
-apt-get install nodejs -y
-apt-get install npm -y
+sudo apt-get update
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
 npm install
 node init-mongo-data.js localhost:27018 mongodb://mongo:password@localhost:27018/dagobah
